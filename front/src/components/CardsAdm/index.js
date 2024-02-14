139fc6888b029375023df413ba2f0a8666b321c9
Marcos <marcoshr_baptista@hotmail.com>
2024-02-14 11:43:33 -0300

    add adm page

--- front/src/components/CardsAdm/index.js
+++ front/src/components/CardsAdm/index.js
@@ -1,25 +1,66 @@
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import UserDefault from './person-circle.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import styles from './styles.module.scss'
+import { Row, Col, Card } from 'react-bootstrap';
 
 export default function CardsAdm() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        GetAllUsers();
+    }, []);
+
+    const GetAllUsers = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/api/user/getAll`);
+
+            if (!response.data) {
+                throw new Error('Erro ao carregar Users da API');
+            }
+
+            const UsersList = response.data.users;
+
+            setUsers(UsersList)
+
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    console.log(users)
+
+
     return (
-        <div className={StyleSheet.divAll}>
-            <Card style={{ width: '18rem', display: 'flex', alignItems: 'center' }}>
-                <Card.Img variant="top" src={UserDefault} style={{ height: '15rem', marginTop: '10px' }} />
-                <Card.Body>
-                    <Card.Title>User Name</Card.Title>
-                    <Card.Text>
-                        <p>
-                            Data da criação:
-                        </p>
-                        <p>
-                            Data de alguma coisa que eu n lembro
-                        </p>
-                    </Card.Text>
-                    <Button variant="danger">Excluir</Button>
-                </Card.Body>
-            </Card>
+        <div className={styles.divAll}>
+            <Row>
+
+                {users
+                    .map((user) => (
+                        <Col>
+                            <Card className={styles.CardsUsers}>
+                                <Card.Img variant="top" src={UserDefault} style={{ height: '15rem', marginTop: '10px' }} />
+                                <Card.Body>
+                                    <Card.Title>{user.email}</Card.Title>
+                                    <Card.Text>
+                                        <p>
+                                            Cadastro:
+                                        </p>
+                                        <p>
+                                            {user.createdAt}
+                                        </p>
+
+                                    </Card.Text>
+                                    <Button variant="danger">Excluir</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+            </Row>
         </div>
     );
 }
+
+
